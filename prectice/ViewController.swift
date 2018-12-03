a2a09942842d9b71664c0da6f99416bddcc31d12
503-25 <a503-25@imac-3333.local>
2018-12-03 10:23:59 +0900

    Initial Commit

---
+++ prectice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prectice
+//
+//  Created by 503-25 on 2018. 12. 3..
+//  Copyright © 2018년 jh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
